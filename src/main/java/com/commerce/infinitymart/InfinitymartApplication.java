15f18467898343ebadbb72e85747de0aca783fd5
Lucas Pontes <48896896+lucasvpontes@users.noreply.github.com>
2024-09-22 18:12:06 -0300

    Initial commit

---
+++ src/main/java/com/commerce/infinitymart/InfinitymartApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.infinitymart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinitymartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinitymartApplication.class, args);
+	}
+
+}
